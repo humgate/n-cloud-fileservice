9d7729c7e707f219132422a5651b87416381d6ac
Pavel Yanovsky <humgate@gmail.com>
2022-05-07 16:13:52 +0300

    Add User and File entities

---
+++ src/main/java/com/humga/cloudservice/entity/File.java
@@ -0,0 +1,24 @@
+package com.humga.cloudservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity @Table(name = "files")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id", nullable = false)
+    private User user;
+
+    @Column(name = "file", nullable = false, columnDefinition = "blob")
+    private byte[] file;
+}
---
+++ src/main/java/com/humga/cloudservice/entity/User.java
@@ -0,0 +1,26 @@
+package com.humga.cloudservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity @Table(name = "users")
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "login", nullable = false)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name= "name")
+    private String name;
+}
