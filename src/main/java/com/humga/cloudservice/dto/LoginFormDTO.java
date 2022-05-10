a920079dfa9b21cf9af90c230b3f9cdbc24a3b98
Pavel Yanovsky <humgate@gmail.com>
2022-05-10 19:00:06 +0300

    Change login validation from @Pattern to built in @Email

--- src/main/java/com/humga/cloudservice/dto/LoginFormDTO.java
+++ src/main/java/com/humga/cloudservice/dto/LoginFormDTO.java
@@ -3,13 +3,12 @@ package com.humga.cloudservice.dto;
 import lombok.Data;
 import org.springframework.validation.annotation.Validated;
 
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Email;
 
 @Data
 @Validated
 public class LoginFormDTO {
-    @Pattern(regexp = "^[A-Za-z0-9._]+[@][A-Za-z0-9.-]+[.][A-Za-z]+$",
-            message = "{login.invalid}")
+    @Email(message = "{login.invalid}")
     private String login;
     private String password;
 }
