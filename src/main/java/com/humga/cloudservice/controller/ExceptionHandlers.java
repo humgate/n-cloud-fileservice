3c316662c68f7c72bb0537375c40b590a4e06965
Pavel Yanovsky <humgate@gmail.com>
2022-05-29 19:52:32 +0300

    Added getting messages from properties using Spring MessageSource

--- src/main/java/com/humga/cloudservice/controller/ExceptionHandlers.java
+++ src/main/java/com/humga/cloudservice/controller/ExceptionHandlers.java
@@ -2,6 +2,7 @@ package com.humga.cloudservice.controller;
 
 import com.humga.cloudservice.model.ErrorDTO;
 import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.context.MessageSource;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,15 +14,22 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Locale;
 import java.util.Objects;
 
 @ControllerAdvice
 public class ExceptionHandlers extends ResponseEntityExceptionHandler {
+    private final MessageSource messageSource;
+    private final Locale locale = Locale.getDefault();
+
+    public ExceptionHandlers(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @ExceptionHandler(value = { BadCredentialsException.class })
     public ResponseEntity<ErrorDTO> handleUnauthorized(BadCredentialsException e) {
 
-        ErrorDTO error = new ErrorDTO("{access-denied}", 100);
+        ErrorDTO error = new ErrorDTO(messageSource.getMessage("bad-credentials", null, locale), 100);
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 
@@ -33,7 +41,8 @@ public class ExceptionHandlers extends ResponseEntityExceptionHandler {
             WebRequest request) {
 
         String message = e.getBindingResult().getAllErrors().stream().map(x -> x.getDefaultMessage())
-                     .filter(Objects::nonNull).reduce(String::concat).orElse("{format.invalid}");
+                .filter(Objects::nonNull).reduce(String::concat).orElse(
+                        messageSource.getMessage("format-invalid", null, locale));
 
         ErrorDTO error = new ErrorDTO(message, 101);
         return handleExceptionInternal(e, error, new HttpHeaders(), status, request);
@@ -46,18 +55,19 @@ public class ExceptionHandlers extends ResponseEntityExceptionHandler {
         if (e.getCause() instanceof ConstraintViolationException) {
             var ex = (ConstraintViolationException) e.getCause();
             if (ex.getConstraintName().equals("uk_files")) {
-                error = new ErrorDTO("{duplicate.object}", 103);
+                error = new ErrorDTO(messageSource.getMessage("duplicate.object", null, locale), 103);
                 return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
             }
         }
-        error = new ErrorDTO("{internal.error}", 102);
+
+        error = new ErrorDTO(messageSource.getMessage("internal.error", null, locale), 102);
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(value = { RuntimeException.class })
-    public ResponseEntity<ErrorDTO> handleRT(RuntimeException e) {
+    public ResponseEntity<ErrorDTO> handleRuntimeException() {
 
-        ErrorDTO error = new ErrorDTO(e.getMessage(), 102);
+        ErrorDTO error = new ErrorDTO(messageSource.getMessage("internal.error", null, locale), 102);
         return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
--- src/main/java/com/humga/cloudservice/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/humga/cloudservice/security/JwtAuthenticationEntryPoint.java
@@ -2,6 +2,7 @@ package com.humga.cloudservice.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.humga.cloudservice.model.ErrorDTO;
+import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -12,10 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
+import java.util.Locale;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
     private final ObjectMapper mapper = new ObjectMapper();
+
+    private final MessageSource messageSource;
+
+    public JwtAuthenticationEntryPoint(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @Override
     public void commence(HttpServletRequest request,
@@ -26,10 +34,9 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
         response.addHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE);
         response.flushBuffer();
 
-        var dto = new ErrorDTO("{access-denied}", 100);
+        var dto = new ErrorDTO(messageSource.getMessage("bad-credentials", null, Locale.getDefault()),100);
         PrintWriter writer = response.getWriter();
         writer.println(mapper.writeValueAsString(dto));
-        writer.flush();
         writer.close();
     }
 }
